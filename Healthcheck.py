e01294459d5a489c4d0308267650aca594e985c2
Nicole Navarrete <glongia618@gmail.com>
2023-09-14 23:00:03 +0000

    Food

--- Healthcheck.py
+++ Healthcheck.py
@@ -7,7 +7,7 @@ added_a_b = a + b
 print(added_a_b)
 print(type(added_a_b))
 
-
+# yay
 print(f"The product of {a} plus {b} is equal too {added_a_b}")
 
 messageApex = 'The product of {0:d} and {1:d} is {2:d}'.format(3, 2, 5)
