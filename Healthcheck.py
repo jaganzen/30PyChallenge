35e4373cc661fd0324b42dbea4ff05bcd15caedf
Nicole Navarrete <glongia618@gmail.com>
2023-09-14 22:41:05 +0000

    testing git w/ .py file

---
+++ Healthcheck.py
@@ -0,0 +1,14 @@
+#addition operation
+
+a = 3
+b = 2
+
+added_a_b = a + b 
+print(added_a_b)
+print(type(added_a_b))
+
+
+print(f"The product of {a} plus {b} is equal too {added_a_b}")
+
+messageApex = 'The product of {0:d} and {1:d} is {2:d}'.format(3, 2, 5)
+print(messageApex)
